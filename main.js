23194f237a4bff04437b1c456f54c2cbf605a696
Laurent Dufloux <laurent.s.dufloux@jyu.fi>
2020-03-25 23:32:05 +0200

    Please

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //import verbs_json from './verbs.json'
 
-verbs_url = "https://github.com/LaurentD87/finngram/blob/master/verbs.json"
-verbs = fetch(verbs_url).then(function (response) { return reponse.json() })
+const verbs = fetch("https://github.com/LaurentD87/finngram/blob/master/verbs.json").then(function (response) { return reponse.json() })
 
 //verbs = JSON.parse(verbs_json)
 console.log(verbs["puhua"])
