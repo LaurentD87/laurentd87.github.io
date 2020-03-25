c47942c6bf06460d5e48a64fee8ddff94a5b3b3e
Laurent Dufloux <laurent.s.dufloux@jyu.fi>
2020-03-25 23:59:22 +0200

    ntm

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 //var verb_url = "https://github.com/LaurentD87/laurentd87.github.io/blob/master/verbs.json"
 verb_url = './verbs.json'
-const verbs = fetch(verb_url).then(function (response) { return response.json() })
+fetch(verb_url).then(function (response) { 
+    verbs = response.json()
+    console.log(verbs["puhua"])
+    
 
-//verbs = JSON.parse(verbs_json)
-console.log(verbs["puhua"])
+    
+})
+
