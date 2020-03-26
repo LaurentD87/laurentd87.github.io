d5c46c99c27e2a1a97113e1926b04b1c1aded855
Laurent Dufloux <laurent.s.dufloux@jyu.fi>
2020-03-26 23:09:13 +0200

    ntm

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-//import verbs_json from './verbs.json'
+verb_url = 'http://127.0.0.1:8887/verbs_list.json'
 
-//var verb_url = "https://github.com/LaurentD87/laurentd87.github.io/blob/master/verbs.json"
-verb_url = './verbs.json'
+
+
+
 fetch(verb_url).then(function (response) { 
     return response.json()
 }).then(function (verbs) {
-    console.log(verbs["puhua"])
+    function getRandomVerb() {
+        return verbs[Math.floor(Math.random() * verbs.length)]
+    }
+
+    function SubmitVerb(verb, verb_data) {
+        requiredVerbSpan = document.getElementById("required-verb")
+        requiredVerbSpan.firstChild.nodeValue = verb
+
+
+        
+
+        answerVerb = document.getElementById("answer-verb")
+        answerVerb.addEventListener("keyup", function (event) {
+            if (event.keyCode == 13) {
+                event.preventDefault()
+                answer = answerVerb.value 
+                if answer == 
+            }
+        })
+    }
+
 })
 
